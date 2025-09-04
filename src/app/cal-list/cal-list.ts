eb8ccbf2875961fa30713bec44670868b6a33bef
Tommy-BOU <tommy.boubaker@free.fr>
2025-09-04 13:35:24 +0200

    add style to the clear list button and ask confirmation before clearing the list

--- src/app/cal-list/cal-list.ts
+++ src/app/cal-list/cal-list.ts
@@ -22,13 +22,13 @@ export class CalList {
    * Called when a filter checkbox is clicked.
    * @param event The click event.
    */
-  applyFilter(event: Event){
+  applyFilter(event: Event) {
     console.log((event.target as HTMLInputElement).value);
     console.log((event.target as HTMLInputElement).checked);
-    if ((event.target as HTMLInputElement).checked && !this.filters.includes((event.target as HTMLInputElement).value)){
+    if ((event.target as HTMLInputElement).checked && !this.filters.includes((event.target as HTMLInputElement).value)) {
       this.filters.push((event.target as HTMLInputElement).value);
     }
-    else{
+    else {
       this.filters.splice(this.filters.indexOf((event.target as HTMLInputElement).value), 1);
     }
     console.log(this.filters);
@@ -37,41 +37,43 @@ export class CalList {
     this.countTotal();
   }
 
-  countTotal(){
+  countTotal() {
     this.total.totalCalories = this.foodList.reduce((total, food) => total + food.calories, 0);
     this.total.totalFoods = this.foodList.length;
   }
 
-  filterList(foods: FoodInfo[], filters: string[]): FoodInfo[]{
-     if (!foods || filters.length === 0){
-        return foods;
+  filterList(foods: FoodInfo[], filters: string[]): FoodInfo[] {
+    if (!foods || filters.length === 0) {
+      return foods;
     }
     return foods.filter((food) => {
-        if (filters.includes('low') && food.calories <= 200){
-            console.log("after : ", foods);
-            return true;
-        }
-        else if (filters.includes('medium') && food.calories > 200 && food.calories <= 500){
-            console.log("after : ", foods);
-            return true;
-        }
-        else if (filters.includes('high') && food.calories > 500){
-            console.log("after : ", foods);
-            return true;
-        }
-        else if (filters.length === 0){
-            console.log("after : ", foods);
-            return true;
-        }
-        else{
-            console.log("after : ", foods);
-            return false;
-        }
+      if (filters.includes('low') && food.calories <= 200) {
+        console.log("after : ", foods);
+        return true;
+      }
+      else if (filters.includes('medium') && food.calories > 200 && food.calories <= 500) {
+        console.log("after : ", foods);
+        return true;
+      }
+      else if (filters.includes('high') && food.calories > 500) {
+        console.log("after : ", foods);
+        return true;
+      }
+      else if (filters.length === 0) {
+        console.log("after : ", foods);
+        return true;
+      }
+      else {
+        console.log("after : ", foods);
+        return false;
+      }
     });
   }
 
-  clearList(){
-    this.foodService.clearList();
+  clearList() {
+    if (confirm('Are you sure you want to clear the list?')) {
+      this.foodService.clearList();
+    }
   }
 }
 
