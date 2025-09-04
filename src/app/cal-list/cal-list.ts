6df38ef56c81fe3167ee78c9b990a061de67c529
Tommy-BOU <tommy.boubaker@free.fr>
2025-09-04 13:19:34 +0200

    remove non working CalorieFilterPipe and filter the foodlist in the ApplyFilter method

--- src/app/cal-list/cal-list.ts
+++ src/app/cal-list/cal-list.ts
@@ -3,16 +3,17 @@ import { FoodService } from '../services/food-service';
 import { FoodInfo } from '../interfaces/food-info';
 import { FoodItem } from '../food-item/food-item';
 import { TotalInfo } from '../interfaces/total-info';
-import { CalorieFilterPipe } from '../pipes/CalorieFilterPipe';
+
 @Component({
   selector: 'app-cal-list',
-  imports: [FoodItem, CalorieFilterPipe],
+  imports: [FoodItem],
   templateUrl: './cal-list.html',
   styleUrl: './cal-list.scss'
 })
 export class CalList {
   foodService: FoodService = inject(FoodService);
-  foodList: FoodInfo[] = this.foodService.getList();
+  originalFoodList: FoodInfo[] = this.foodService.getList();
+  foodList: FoodInfo[] = this.originalFoodList;
   total: TotalInfo = this.foodService.getTotal();
   filters: string[] = [];
 
@@ -30,6 +31,36 @@ export class CalList {
       this.filters.splice(this.filters.indexOf((event.target as HTMLInputElement).value), 1);
     }
     console.log(this.filters);
+    this.foodList = this.originalFoodList;
+    this.foodList = this.filterList(this.foodList, this.filters);
+  }
+
+  filterList(foods: FoodInfo[], filters: string[]): FoodInfo[]{
+     if (!foods || filters.length === 0){
+        return foods;
+    }
+    return foods.filter((food) => {
+        if (filters.includes('low') && food.calories <= 200){
+            console.log("after : ", foods);
+            return true;
+        }
+        else if (filters.includes('medium') && food.calories > 200 && food.calories <= 500){
+            console.log("after : ", foods);
+            return true;
+        }
+        else if (filters.includes('high') && food.calories > 500){
+            console.log("after : ", foods);
+            return true;
+        }
+        else if (filters.length === 0){
+            console.log("after : ", foods);
+            return true;
+        }
+        else{
+            console.log("after : ", foods);
+            return false;
+        }
+    });
   }
 
   clearList(){
