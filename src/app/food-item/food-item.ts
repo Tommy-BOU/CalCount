6e7dbcc6f9982d39da44b7145c480b1e25f4271f
Tommy-BOU <tommy.boubaker@free.fr>
2025-09-05 13:38:46 +0200

    improve frontend of food list

--- src/app/food-item/food-item.ts
+++ src/app/food-item/food-item.ts
@@ -1,16 +1,18 @@
-import { Component, input, inject } from '@angular/core';
+import { Component, input, inject, signal } from '@angular/core';
 import { FoodInfo } from '../interfaces/food-info';
 import { FoodService } from '../services/food-service';
+import { DatePipe } from '@angular/common';
+import { NgClass } from '@angular/common';
+
 @Component({
   selector: 'app-food-item',
-  imports: [],
+  imports: [DatePipe, NgClass],
   templateUrl: './food-item.html',
   styleUrl: './food-item.scss'
 })
 export class FoodItem {
   foodService = inject(FoodService);
   food = input.required<FoodInfo>();
-  componentRef = document.getElementById('food-item');
 
   removeFood() {
     if (confirm('Are you sure you want to remove this food?')) {
